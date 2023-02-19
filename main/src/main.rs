13fd8cc6b95bc9745aa58a5aeafc6f51dfd6bc7b
Deimos <deimoshall@gmail.com>
2023-02-19 17:50:53 -0600

    refactor: change the name of the function from get_random_something to get_something

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,22 +1,25 @@
 use random_str as random;
 
 fn main() {
-    let random_letter = random::get_random_letter(true, true);
+    let random_letter = random::get_letter(true, true);
     println!("Random letter: {}", random_letter);
 
-    let random_symbol = random::get_random_symbol();
+    let random_symbol = random::get_symbol();
     println!("Random symbol: {}", random_symbol);
 
-    let random_number = random::get_random_int(0, 9);
+    let random_number = random::get_int(0, 9);
     println!("Random number: {}", random_number);
 
-    let seven_digits = random::get_random_int(1000000, 9999999); // 7 digits
+    let seven_digits = random::get_int(1000000, 9999999); // 7 digits
     let random_phone_number = format!("+52 343{}", seven_digits);
     println!("Random phone number: {}", random_phone_number);
 
-    let random_password = random::get_random_string(16, true, true, true, true);
+    let random_password = random::get_string(16, true, true, true, true);
     println!("Random password: {}", random_password);
 
-    let random_bool = random::get_random_bool();
+    let random_bool = random::get_bool();
     println!("Random bool: {}", random_bool);
+
+    let random_char = random::get_char();
+    println!("Random char: {}", random_char);
 }
--- random_str/src/lib.rs
+++ random_str/src/lib.rs
@@ -1,9 +1,10 @@
+//! This crate provides a set of functions to generate random strings, numbers, letters, symbols and booleans.
 use rand::prelude::*;
 
 /// Get a random char from a-z or A-Z
 /// 
 /// # Panics
-/// Panics if the function is called with both parameters set to false.
+/// Panics if the function is called with both parameters set to false
 ///
 /// # Examples
 ///
@@ -12,22 +13,22 @@ use rand::prelude::*;
 /// ```
 /// 
 /// ```
-/// let random_letter = random::get_random_letter(true, false);
+/// let random_letter = random::get_letter(true, false);
 /// println!("Random letter: {}", random_char);
 /// ```
 /// Possible output: Random letter: w
 /// ```
-/// let random_letter = random::get_random_letter(false, true);
+/// let random_letter = random::get_letter(false, true);
 /// println!("Random letter: {}", random_letter);
 /// ```
 /// Possible output: Random letter: X
 /// ```
-/// let random_letter = random::get_random_letter(true, true);
+/// let random_letter = random::get_letter(true, true);
 /// println!("Random letter: {}", random_letter);
 /// ```
 /// Possible output: Random letter: x or Random letter: Y
 #[cfg(not(doctest))]
-pub fn get_random_letter(lowercase: bool, uppercase: bool) -> char {
+pub fn get_letter(lowercase: bool, uppercase: bool) -> char {
     let mut chars: Vec<char> = vec![];
 
     if lowercase {
@@ -45,19 +46,63 @@ pub fn get_random_letter(lowercase: bool, uppercase: bool) -> char {
     *random_char
 }
 
-pub fn get_random_int(min: i32, max: i32) -> i32 {
+/// Get a random number (i32) from a minimum and maximum value
+/// 
+/// # Panics
+/// Panics if min is greater than max
+/// 
+/// # Examples
+/// 
+/// ```
+/// use random_str as random;
+/// ```
+/// ```
+/// let random_number = random::get_int(0, 9);
+/// println!("Random number: {}", random_number);
+/// ```
+/// Possible output: Random number: 3
+#[cfg(not(doctest))]
+pub fn get_int(min: i32, max: i32) -> i32 {
     let mut rng = thread_rng();
     rng.gen_range(min..=max)
 }
 
-pub fn get_random_symbol() -> char {
+/// Get a random symbol from a list of symbols
+/// Possible symbols are: #, $, %, &, *, @, ^
+/// 
+/// # Examples
+/// 
+/// ```
+/// use random_str as random;
+/// ```
+/// ```
+/// let random_symbol = random::get_symbol();
+/// println!("Random symbol: {}", random_symbol);
+/// ```
+#[cfg(not(doctest))]
+pub fn get_symbol() -> char {
     let symbols: Vec<char> = vec!['#', '$', '%', '&', '*', '@', '^'];
     let mut rng = thread_rng();
     let random_symbol = symbols.choose(&mut rng).unwrap();
     *random_symbol
 }
 
-pub fn get_random_string(length: usize, lowercase: bool, uppercase: bool, number: bool, symbol: bool) -> String {
+/// Get a random string with a given length and a set of characters
+/// 
+/// # Panics
+/// Panics if the function is called with all parameters set to false
+/// 
+/// # Examples
+/// 
+/// ```
+/// use random_str as random;
+/// ```
+/// ```
+/// let random_password = random::get_string(16, true, true, true, true);
+/// println!("Random password: {}", random_password);
+/// ```
+#[cfg(not(doctest))]
+pub fn get_string(length: usize, lowercase: bool, uppercase: bool, number: bool, symbol: bool) -> String {
     let symbols_list: Vec<char> = vec!['#', '$', '%', '&', '*', '@', '^'];
     let numbers_list: Vec<char> = (b'0'..b'9').map(|c| c as char).collect();
     let cap_letters_list: Vec<char> = (b'A'..b'Z').map(|c| c as char).collect();
@@ -81,9 +126,28 @@ pub fn get_random_string(length: usize, lowercase: bool, uppercase: bool, number
     (0..length).map(|_| *random_string.choose(&mut rng).unwrap()).collect()
 }
 
-pub fn get_random_bool() -> bool {
+/// Get a random boolean value
+/// 
+/// # Examples
+/// 
+/// ```
+/// use random_str as random;
+/// ```
+/// ```
+/// let random_bool = random::get_bool();
+/// println!("Random boolean: {}", random_bool);
+/// ```
+/// Possible output: Random boolean: true or Random boolean: false
+#[cfg(not(doctest))]
+pub fn get_bool() -> bool {
     let mut rng = thread_rng();
     rng.gen_bool(0.5)
+}
+
+pub fn get_char() -> char {
+    let chars: Vec<char> = (b'!'..b'~').map(|c| c as char).collect();
+    let mut rng = thread_rng();
+    chars.choose(&mut rng).unwrap().clone()
 }
 
 #[cfg(test)]
@@ -92,28 +156,28 @@ mod tests {
 
     #[test]
     fn test_get_random_lowercase_letter() {
-        let random_letter = get_random_letter(true, false);
+        let random_letter = get_letter(true, false);
         assert!(random_letter.is_ascii_alphabetic());
         assert!(random_letter.is_ascii_lowercase());
     }
 
     #[test]
     fn test_get_random_uppercase_letter() {
-        let random_letter = get_random_letter(false, true);
+        let random_letter = get_letter(false, true);
         assert!(random_letter.is_ascii_alphabetic());
         assert!(random_letter.is_ascii_uppercase());
     }
 
     #[test]
     fn test_get_random_letter() {
-        let random_letter = get_random_letter(true, true);
+        let random_letter = get_letter(true, true);
         assert!(random_letter.is_ascii_alphabetic());
     }
 
     #[test]
     // Validate if the function panics when both parameters are false
     fn test_get_random_false_letter() {
-        let result = std::panic::catch_unwind(|| get_random_letter(false, false));
+        let result = std::panic::catch_unwind(|| get_letter(false, false));
         assert!(result.is_err());
     }
 }
