4dd344108fc26f2d2899e6c2e310417c1f0336f7
Deimos <deimoshall@gmail.com>
2023-06-20 10:51:15 -0600

    refactor: replace magic values with variables

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,20 +1,30 @@
 use random_str as random;
 
 fn main() {
-    let random_letter = random::get_letter(true, true);
+    let lowercase = true;
+    let uppercase = true;
+
+    let random_letter = random::get_letter(lowercase, uppercase);
     println!("Random letter: {}", random_letter);
 
     let random_symbol = random::get_symbol();
     println!("Random symbol: {}", random_symbol);
 
-    let random_number = random::get_int(0, 9);
+    let mut min = 0;
+    let mut max = 9;
+    let random_number = random::get_int(min, max);
     println!("Random number: {}", random_number);
 
-    let seven_digits = random::get_int(1000000, 9999999); // 7 digits
+    min = 1000000;
+    max = 9999999;
+    let seven_digits = random::get_int(min, max); // 7 digits
     let random_phone_number = format!("+52 343{}", seven_digits);
     println!("Random phone number: {}", random_phone_number);
 
-    let random_password = random::get_string(16, true, true, true, true);
+    let length = 16;
+    let numbers = true;
+    let symbols = true;
+    let random_password = random::get_string(length, lowercase, uppercase, numbers, symbols);
     println!("Random password: {}", random_password);
 
     let random_bool = random::get_bool();
