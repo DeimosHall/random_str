caea4bf66a65a7ad2bc37a9bbd56a3ba92e7aaa8
deimos <deimoshall.dev@proton.me>
2026-02-10 08:47:48 -0600

    refactor: add explicit types

--- main/src/main.rs
+++ main/src/main.rs
@@ -18,14 +18,14 @@ fn main() {
     let random_number = random::get_int(min, max); // Between 0 and 9
     println!("Random number: {}", random_number);
 
-    let digits = RandomStringBuilder::new()
+    let digits: Option<String> = RandomStringBuilder::new()
         .with_length(10)
         .with_numbers()
         .build();
     let random_phone_number = format!("+52 {}", digits.unwrap());
     println!("Random phone number: {}", random_phone_number);
 
-    let random_password = RandomStringBuilder::new()
+    let random_password: Option<String> = RandomStringBuilder::new()
         .with_length(32)  // Optional, 16 as default
         .with_lowercase()
         .with_uppercase()
